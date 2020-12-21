0331b5e6082778a2e4a4be277cad142532d506d6
Wiifou <mael.bouchand@isen.yncrea.fr>
2020-12-21 15:41:58 +0100

    lang component

--- TheSocialNetJoke/src/app/settings/settings.component.ts
+++ TheSocialNetJoke/src/app/settings/settings.component.ts
@@ -4,9 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'cnt-settings',
   template: `
 
-  <p>
-  settings works!
-</p>
+<h2>Language Settings</h2>
+  <p><button type="button" class="btn btn-primary">French</button></p>
+  <p><button type="button" class="btn btn-primary">English</button></p>
+  <p><button type="button" class="btn btn-primary">German</button></p>
+  <p><button type="button" class="btn btn-primary">Spanish</button></p>
+  <p><button type="button" class="btn btn-primary">Czech</button></p>
+  <p></p>
+<h2>Blacklist</h2>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked>
+  <label class="form-check-label" for="flexSwitchCheck">nsfw</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+  <label class="form-check-label" for="flexSwitchCheck">religious</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+  <label class="form-check-label" for="flexSwitchCheck">political</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+  <label class="form-check-label" for="flexSwitchCheck">religious</label>
+</div>
 
    
   `,
